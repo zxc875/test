75bcf667f5b928eb686b04d0818385e89bef24e9
Binglin <576238381@qq.com>
2022-09-09 16:29:21 +0800

    111

--- x.py
+++ x.py
@@ -1,28 +1,75 @@
-from tkinter import *
+# -*- encoding=utf-8 -*-
+import serial
+import time
 
-root = Tk()
+#import WriteLog
 
-w = Label(root, text="Right-click to display menu", width=40, height=20)
-w.pack()
 
-# create a menu
-popup = Menu(root, tearoff=0)
-popup.add_command(label="Next") # , command=next) etc...
-popup.add_command(label="Previous")
-popup.add_separator()
-popup.add_command(label="Home")
+class COM:
+    def __init__(self, port, baud):
+        self.port = port
+        self.baud = int(baud)
+        self.open_com = None
+        #self.log = WriteLog.WriteLog('ITC_LOG.LOG')
+        self.get_data_flag = True
+        self.real_time_data = ''
 
-def do_popup(event):
-    # display the popup menu
-    try:
-        popup.tk_popup(event.x_root, event.y_root, 0)
-    finally:
-        # make sure to release the grab (Tk 8.0a1 only)
-        popup.grab_release()
+    # return real time data form com
+    def get_real_time_data(self):
+        return self.real_time_data
 
-w.bind("<Button-3>", do_popup)
+    def clear_real_time_data(self):
+        self.real_time_data = ''
 
-b = Button(root, text="Quit", command=root.destroy)
-b.pack()
+    # set flag to receive data or not
+    def set_get_data_flag(self, get_data_flag):
+        self.get_data_flag = get_data_flag
 
-mainloop()
+    def open(self):
+        try:
+            self.open_com = serial.Serial(self.port, self.baud)
+        except Exception as e:
+            print(1)
+            #self.log.error('Open com fail:{}/{}'.format(self.port, self.baud))
+            #self.log.error('Exception:{}'.format(e))
+
+    def close(self):
+        if self.open_com is not None and self.open_com.isOpen:
+            self.open_com.close()
+
+    def send_data(self, data):
+        if self.open_com is None:
+            self.open()
+        success_bytes = self.open_com.write(data.encode('UTF-8'))
+        return success_bytes
+
+    def get_data(self, over_time=30):
+        all_data = ''
+        if self.open_com is None:
+            self.open()
+        start_time = time.time()
+        while True:
+            end_time = time.time()
+            if end_time - start_time < over_time and self.get_data_flag:
+                data = self.open_com.read(self.open_com.inWaiting())
+                # data = self.open_com.read() # read 1 size
+                data = str(data.decode('UTF-8'))
+                if data != '':
+                    #self.log.info('Get data is:{}'.format(data))
+                    all_data = all_data + data
+                    print(data)
+                    self.real_time_data = all_data
+                    #print(all_data)
+            else:
+                self.set_get_data_flag(True)
+                break
+        return all_data
+
+
+if __name__ == '__main__':
+    pass
+    com = COM('com3', 9600)
+    # com.open()
+    #print(com.send_data('data'))
+    com.get_data(10)
+    com.close()
